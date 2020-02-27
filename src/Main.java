14699cc970dcaf5d0d8ec025b1b8ff3687c7efcd
wesley <fwsantos23@gmail.com>
2020-02-26 22:56:58 -0300

    files read

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Arrays; // Import the Arrays class to sort array
+import java.util.Scanner; // Import the Scanner class to read text files
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Project created.");
-    }
+	public static void main(String[] args) {
+		try {	
+			File folder = new File("cos_tardiness_SMALL/");
+			String[] files = folder.list();
+			Arrays.sort(files);
+			Scanner sc;
+			int aInt;
+
+			for (String file : files)
+			{
+				System.out.println(file);
+
+				sc = new Scanner(new File("cos_tardiness_SMALL/"+file));
+				while (sc.hasNextInt()) {
+					aInt = sc.nextInt();
+					// System.out.println(aInt); 
+				}
+
+				sc.close();
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+
+
+	}
 }
--- src/OS.java
+++ src/OS.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+//CLass for Order Scheduling
+
+import java.util.ArrayList;
 
 public class OS {
+    private int M;
+    private int N;
+    private ArrayList<Integer> jobs = new ArrayList<Integer>();
+
+    public int getM() {
+        return M;
+    }
+
+    public void setM(int m) {
+        M = m;
+    }
+
+    public int getN() {
+        return N;
+    }
+
+    public void setN(int n) {
+        N = n;
+    }
+
 }
